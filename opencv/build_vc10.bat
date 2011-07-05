17314ba04dac5f324270c2dea3ed84ec10b95fe3
fsi-katami <fsi-katami@71d6d17e-f831-4cb6-a2e6-a089888e0ea0>
2011-07-05 06:42:24 +0000

    For vc2010. refs #1915
    git-svn-id: https://tmp.openrtm.org/svn/ImageProcessing/trunk@55 71d6d17e-f831-4cb6-a2e6-a089888e0ea0

--- opencv/build_vc10.bat
+++ opencv/build_vc10.bat
@@ -9,6 +9,14 @@
 @rem   + Please set path of cmake and vc2010.  
 @rem
 
+@set OPENCV_ROOT=C:\distribution\OpenCV2.2
+@set WindowsSdkDir=
+@for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v "CurrentInstallFolder"') DO (
+	if "%%i"=="CurrentInstallFolder" (
+		set "WindowsSdkDir=%%k"
+	)
+)
+
 @set PATH="C:\Program Files\CMake 2.8\bin";"C:\Program Files\Microsoft Visual Studio 10.0\VC\BIN";"c:\WINDOWS\Microsoft.NET\Framework\v4.0.30319";"C:\Program Files\Microsoft Visual Studio 10.0\VC\vcpackages";%PATH%
 
 echo Generating SoLutio files / WorkSpace.
