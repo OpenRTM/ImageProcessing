cc2186749176a0b64682dbb66361d959bf9164e9
kawauchi <kawauchi@71d6d17e-f831-4cb6-a2e6-a089888e0ea0>
2015-07-23 06:32:02 +0000

    Binary zip package name has been changed.
    git-svn-id: https://tmp.openrtm.org/svn/ImageProcessing/trunk@174 71d6d17e-f831-4cb6-a2e6-a089888e0ea0

--- opencv/ip_make_package.sh
+++ opencv/ip_make_package.sh
@@ -7,35 +7,13 @@
 # This is part of ip-build.bat OpenCVRTC build batch file
 # for building ImageProcessing for Windows binaries.
 #
-# The following environment variables should be set.
-# ex.
-# export PYTHON_DIR=/cygdrive/c/Python27
-# export VC_VERSION=9
-# export ARCH=x86
-#
 #==============================
 
 WORK_ROOT=`pwd`
 echo "Current dir:"  ${WORK_ROOT}
 
-if test "x$ARCH" = "xx86" ; then
-	WIN_ARCH=win32
-elif test "x$ARCH" = "xx86_64" ; then
-	WIN_ARCH=win64
-else
-	echo "Unknown architecture: " $ARCH
-	exit -1
-fi
-
-if test "x$PYTHON_DIR" = "x" ; then
-	echo "PYTHON_DIR is not defined. "
-	exit -1
-else
-	PY_VER=`echo $PYTHON_DIR | sed 's/.*[Pp][Yy][Tt][Hh][Oo][Nn]\([0-9][0-9]\).*/\1/'`
-fi
-
-NEW_DIR=$ZIP_DIR-${WIN_ARCH}-vc${VC_VERSION}
-ZIP_FILE=$ZIP_DIR-${WIN_ARCH}-vc${VC_VERSION}-py${PY_VER}.zip
+NEW_DIR=opencv_rtcs
+ZIP_FILE=${NEW_DIR}.zip
 
 echo "NEW_DIR: " ${NEW_DIR}
 echo "ZIP_FILE: " ${ZIP_FILE}
@@ -55,7 +33,6 @@ chmod -R 755 ${NEW_DIR}
 
 rm -rf ${ZIP_FILE}
 zip -r ${ZIP_FILE} ${NEW_DIR}
-rm -rf ${NEW_DIR}
 
 # end of script
 #==============================
