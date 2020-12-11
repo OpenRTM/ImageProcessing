73b4b66b0fe45aefa60807cdffd350f3ac7f83f6
n-kawauchi <n.kawauchi@aist.go.jp>
2020-12-11 17:32:25 +0900

    Regenerated cmake file for Binarization.

--- opencv/components/Binarization/src/Binarization.cpp
+++ opencv/components/Binarization/src/Binarization.cpp
@@ -1,8 +1,10 @@
-// -*- C++ -*-
+﻿// -*- C++ -*-
 /*!
  * @file  Binarization.cpp
  * @brief Binarization image component
  * @date $Date$
+ *
+ * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * $Id$
  */
@@ -18,9 +20,9 @@ static const char* binarization_spec[] =
     "implementation_id", "Binarization",
     "type_name",         "Binarization",
     "description",       "Binarization image component",
-    "version",           "1.2.0",
+    "version",           "1.2.3",
     "vendor",            "AIST",
-    "category",          "Category",
+    "category",          "opencv-rtcs",
     "activity_type",     "PERIODIC",
     "kind",              "DataFlowComponent",
     "max_instance",      "1",
@@ -28,10 +30,14 @@ static const char* binarization_spec[] =
     "lang_type",         "compile",
     // Configuration variables
     "conf.default.threshold_level", "128",
+
     // Widget
     "conf.__widget__.threshold_level", "slider.1",
     // Constraints
     "conf.__constraints__.threshold_level", "0<=x<=255",
+
+    "conf.__type__.threshold_level", "int",
+
     ""
   };
 // </rtc-template>
@@ -58,29 +64,30 @@ Binarization::~Binarization()
 }
 
 
+
 RTC::ReturnCode_t Binarization::onInitialize()
 {
   // Registration: InPort/OutPort/Service
   // <rtc-template block="registration">
   // Set InPort buffers
   addInPort("original_image", m_image_origIn);
-  
+
   // Set OutPort buffer
   addOutPort("output_image", m_image_outputOut);
-  
+
   // Set service provider to Ports
-  
+
   // Set service consumers to Ports
-  
+
   // Set CORBA Service Ports
-  
+
   // </rtc-template>
 
   // <rtc-template block="bind_config">
   // Bind variables and configuration variable
   bindParameter("threshold_level", m_nThresholdLv, "128");
   // </rtc-template>
-  
+
   return RTC::RTC_OK;
 }
 
@@ -108,18 +115,12 @@ RTC::ReturnCode_t Binarization::onShutdown(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t Binarization::onActivated(RTC::UniqueId ec_id)
 {
-
-
-
-
   return RTC::RTC_OK;
 }
 
 
 RTC::ReturnCode_t Binarization::onDeactivated(RTC::UniqueId ec_id)
 {
-
-
   return RTC::RTC_OK;
 }
 
@@ -133,45 +134,41 @@ RTC::ReturnCode_t Binarization::onExecute(RTC::UniqueId ec_id)
     /* InPortデータの読み込み */
     m_image_origIn.read();
 
-
-
     /* InPortの画像データをIplImageのimageDataにコピー */
     //memcpy(m_image_buff.data,(void *)&(m_image_orig.pixels[0]),m_image_orig.pixels.length());
-	cv::Mat m_image_buff(cv::Size(m_image_orig.width, m_image_orig.height), CV_8UC3, (void *)&(m_image_orig.pixels[0]));
-	cv::Mat m_image_gray;         // Grayscale image
-	cv::Mat m_image_binary;       // Binary image
-	cv::Mat m_image_dest;         // 結果出力用IplImage
+    cv::Mat m_image_buff(cv::Size(m_image_orig.width, m_image_orig.height), CV_8UC3, (void *)&(m_image_orig.pixels[0]));
+    cv::Mat m_image_gray;         // Grayscale image
+    cv::Mat m_image_binary;       // Binary image
+    cv::Mat m_image_dest;         // 結果出力用IplImage
 
     /* Anternative process */
     /* BGRからグレースケールに変換する */
-	cv::cvtColor(m_image_buff, m_image_gray, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(m_image_buff, m_image_gray, cv::COLOR_BGR2GRAY);
 
     /* グレースケールから2値に変換する */
-	cv::threshold(m_image_gray, m_image_binary, m_nThresholdLv, THRESHOLD_MAX_VALUE, cv::THRESH_BINARY);
+    cv::threshold(m_image_gray, m_image_binary, m_nThresholdLv, THRESHOLD_MAX_VALUE, cv::THRESH_BINARY);
 
-	std::vector<cv::Mat> tmp;
-	tmp.push_back(m_image_binary);
-	tmp.push_back(m_image_binary);
-	tmp.push_back(m_image_binary);
+    std::vector<cv::Mat> tmp;
+    tmp.push_back(m_image_binary);
+    tmp.push_back(m_image_binary);
+    tmp.push_back(m_image_binary);
     /* Convert to 3channel image */
-	cv::merge(tmp, m_image_dest);
+    cv::merge(tmp, m_image_dest);
 
     /* Common process */
     /* 画像データのサイズ取得 */
-	int len = m_image_dest.channels() * m_image_dest.size().width * m_image_dest.size().height;
+    int len = m_image_dest.channels() * m_image_dest.size().width * m_image_dest.size().height;
           
     /* 画面のサイズ情報を入れる */
     m_image_output.pixels.length(len);        
-	m_image_output.width = m_image_dest.size().width;
-	m_image_output.height = m_image_dest.size().height;
+    m_image_output.width = m_image_dest.size().width;
+    m_image_output.height = m_image_dest.size().height;
 
     /* 反転した画像データをOutPortにコピー */
     memcpy((void *)&(m_image_output.pixels[0]), m_image_dest.data,len);
 
     /* 反転した画像データをOutPortから出力する */
     m_image_outputOut.write();
-
-    //cvWaitKey( 0 );
   }
 
   return RTC::RTC_OK;
@@ -216,7 +213,7 @@ RTC::ReturnCode_t Binarization::onRateChanged(RTC::UniqueId ec_id)
 
 extern "C"
 {
- 
+
   void BinarizationInit(RTC::Manager* manager)
   {
     coil::Properties profile(binarization_spec);
@@ -224,7 +221,7 @@ extern "C"
                              RTC::Create<Binarization>,
                              RTC::Delete<Binarization>);
   }
-  
+
 };
 
 
