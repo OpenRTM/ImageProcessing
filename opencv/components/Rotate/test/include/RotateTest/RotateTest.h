42f4ebeb3b6eb5c10fb16f626deb088cf6e7a899
n-kawauchi <n.kawauchi@aist.go.jp>
2021-01-05 16:39:39 +0900

    Regenerated cmake file for Rotate.

--- opencv/components/Rotate/test/include/RotateTest/RotateTest.h
+++ opencv/components/Rotate/test/include/RotateTest/RotateTest.h
@@ -1,6 +1,6 @@
 ﻿// -*- C++ -*-
 /*!
- * @file  Rotate.h
+ * @file  RotateTest.h
  * @brief Rotate image component
  * @date  $Date$
  *
@@ -9,25 +9,26 @@
  * $Id$
  */
 
-#ifndef ROTATE_H
-#define ROTATE_H
+#ifndef ROTATE_TEST__H
+#define ROTATE_TEST_H
 
 #include <rtm/idl/BasicDataTypeSkel.h>
 #include <rtm/idl/ExtendedDataTypesSkel.h>
 #include <rtm/idl/InterfaceDataTypesSkel.h>
 
-/* OpenCV用インクルードファイルのインクルード */
-#include <opencv2/opencv.hpp>
-
 // Service implementation headers
 // <rtc-template block="service_impl_h">
+#include "InterfaceDataTypesSVC_impl.h"
 
 // </rtc-template>
 
 // Service Consumer stub headers
 // <rtc-template block="consumer_stub_h">
-#include "InterfaceDataTypesStub.h"
 
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="port_stub_h">
 // </rtc-template>
 
 #include <rtm/Manager.h>
@@ -37,11 +38,11 @@
 #include <rtm/DataOutPort.h>
 
 /*!
- * @class Rotate
+ * @class RotateTest
  * @brief Rotate image component
  *
  */
-class Rotate
+class RotateTest
   : public RTC::DataFlowComponentBase
 {
  public:
@@ -49,12 +50,12 @@ class Rotate
    * @brief constructor
    * @param manager Maneger Object
    */
-  Rotate(RTC::Manager* manager);
+  RotateTest(RTC::Manager* manager);
 
   /*!
    * @brief destructor
    */
-  ~Rotate();
+  ~RotateTest();
 
   // <rtc-template block="public_attribute">
   
@@ -67,6 +68,7 @@ class Rotate
   /***
    *
    * The initialize action (on CREATED->ALIVE transition)
+   * formaer rtc_init_entry() 
    *
    * @return RTC::ReturnCode_t
    * 
@@ -77,6 +79,7 @@ class Rotate
   /***
    *
    * The finalize action (on ALIVE->END transition)
+   * formaer rtc_exiting_entry()
    *
    * @return RTC::ReturnCode_t
    * 
@@ -87,6 +90,7 @@ class Rotate
   /***
    *
    * The startup action when ExecutionContext startup
+   * former rtc_starting_entry()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -99,6 +103,7 @@ class Rotate
   /***
    *
    * The shutdown action when ExecutionContext stop
+   * former rtc_stopping_entry()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -111,6 +116,7 @@ class Rotate
   /***
    *
    * The activated action (Active state entry action)
+   * former rtc_active_entry()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -123,6 +129,7 @@ class Rotate
   /***
    *
    * The deactivated action (Active state exit action)
+   * former rtc_active_exit()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -135,6 +142,7 @@ class Rotate
   /***
    *
    * The execution action that is invoked periodically
+   * former rtc_active_do()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -147,6 +155,7 @@ class Rotate
   /***
    *
    * The aborting action when main logic error occurred.
+   * former rtc_aborting_entry()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -159,6 +168,7 @@ class Rotate
   /***
    *
    * The error action in ERROR state
+   * former rtc_error_do()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -171,6 +181,7 @@ class Rotate
   /***
    *
    * The reset action that is invoked resetting
+   * This is same but different the former rtc_init_entry()
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -183,6 +194,7 @@ class Rotate
   /***
    *
    * The state update action that is invoked after onExecute() action
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -195,6 +207,7 @@ class Rotate
   /***
    *
    * The action that is invoked when execution context's rate is changed
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
    *
    * @param ec_id target ExecutionContext Id
    *
@@ -233,20 +246,20 @@ class Rotate
 
   // DataInPort declaration
   // <rtc-template block="inport_declare">
-  RTC::CameraImage m_image_orig;
+  RTC::CameraImage m_image_output;
   /*!
    */
-  RTC::InPort<RTC::CameraImage> m_image_origIn;
+  RTC::InPort<RTC::CameraImage> m_image_outputIn;
   
   // </rtc-template>
 
 
   // DataOutPort declaration
   // <rtc-template block="outport_declare">
-  RTC::CameraImage m_image_output;
+  RTC::CameraImage m_image_orig;
   /*!
    */
-  RTC::OutPort<RTC::CameraImage> m_image_outputOut;
+  RTC::OutPort<RTC::CameraImage> m_image_origOut;
   
   // </rtc-template>
 
@@ -274,13 +287,12 @@ class Rotate
   
   // </rtc-template>
 
-  cv::Mat m_transformMatrix;
 };
 
 
 extern "C"
 {
-  DLL_EXPORT void RotateInit(RTC::Manager* manager);
+  DLL_EXPORT void RotateTestInit(RTC::Manager* manager);
 };
 
-#endif // ROTATE_H
+#endif // ROTATE_TEST_H
---
+++ opencv/components/Rotate/test/src/RotateTest.cpp
@@ -0,0 +1,184 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RotateTest.cpp
+ * @brief Rotate image component
+ * @date $Date$
+ *
+ * @author Noriaki Ando <n-ando@aist.go.jp>
+ *
+ * $Id$
+ */
+
+#include "RotateTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* rotate_spec[] =
+  {
+    "implementation_id", "RotateTest",
+    "type_name",         "RotateTest",
+    "description",       "Rotate image component",
+    "version",           "1.2.3",
+    "vendor",            "AIST",
+    "category",          "opencv-rtcs",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.rotate_angle", "60",
+    "conf.default.scale", "0.8",
+
+    // Widget
+    "conf.__widget__.rotate_angle", "text",
+    "conf.__widget__.scale", "text",
+    // Constraints
+
+    "conf.__type__.rotate_angle", "double",
+    "conf.__type__.scale", "double",
+
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RotateTest::RotateTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_image_origIn("original_image", m_image_orig),
+    m_image_outputOut("output_image", m_image_output)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RotateTest::~RotateTest()
+{
+}
+
+
+
+RTC::ReturnCode_t RotateTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("output_image", m_image_outputIn);
+
+  // Set OutPort buffer
+  addOutPort("original_image", m_image_origOut);
+
+  // Set service provider to Ports
+
+  // Set service consumers to Ports
+
+  // Set CORBA Service Ports
+
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("rotate_angle", m_dbRotate, "60");
+  bindParameter("scale", m_dbScale, "0.8");
+  // </rtc-template>
+
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RotateTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t RotateTest::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RotateTest::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RotateTest::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RotateTest::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RotateTest::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+
+  void RotateTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(rotate_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<RotateTest>,
+                             RTC::Delete<RotateTest>);
+  }
+
+};
+
+
