cb419128e62ba095ddbfd907753597b7ea982ca1
n-kawauchi <n.kawauchi@aist.go.jp>
2021-01-08 15:20:05 +0900

    Regenerated cmake file for Translate.

--- opencv/components/Translate/src/Translate.cpp
+++ opencv/components/Translate/src/Translate.cpp
@@ -1,8 +1,14 @@
-// -*- C++ -*-
+﻿// -*- C++ -*-
 /*!
  * @file  Translate.cpp
  * @brief Translate image component
  * @date $Date$
+ *
+ * This RT-Component source code is using the code in
+ * "OpenCVプログラミングブック" (OpenCV Programming book).
+ * Please refer: https://book.mynavi.jp/support/pc/opencv11/#F_DWN
+ *
+ * @author Noriaki Ando <n-ando@aist.go.jp>
  *
  * $Id$
  */
@@ -16,9 +22,9 @@ static const char* translate_spec[] =
     "implementation_id", "Translate",
     "type_name",         "Translate",
     "description",       "Translate image component",
-    "version",           "1.2.0",
+    "version",           "1.2.3",
     "vendor",            "AIST",
-    "category",          "Category",
+    "category",          "opencv-rtcs",
     "activity_type",     "PERIODIC",
     "kind",              "DataFlowComponent",
     "max_instance",      "1",
@@ -27,10 +33,15 @@ static const char* translate_spec[] =
     // Configuration variables
     "conf.default.Translate_x", "50",
     "conf.default.Translate_y", "50",
+
     // Widget
     "conf.__widget__.Translate_x", "text",
     "conf.__widget__.Translate_y", "text",
     // Constraints
+
+    "conf.__type__.Translate_x", "int",
+    "conf.__type__.Translate_y", "int",
+
     ""
   };
 // </rtc-template>
@@ -64,16 +75,16 @@ RTC::ReturnCode_t Translate::onInitialize()
   // <rtc-template block="registration">
   // Set InPort buffers
   addInPort("original_image", m_image_origIn);
-  
+
   // Set OutPort buffer
   addOutPort("output_image", m_image_outputOut);
-  
+
   // Set service provider to Ports
-  
+
   // Set service consumers to Ports
-  
+
   // Set CORBA Service Ports
-  
+
   // </rtc-template>
 
   // <rtc-template block="bind_config">
@@ -81,7 +92,7 @@ RTC::ReturnCode_t Translate::onInitialize()
   bindParameter("Translate_x", m_nTransX, "50");
   bindParameter("Translate_y", m_nTransY, "50");
   // </rtc-template>
-  
+
   return RTC::RTC_OK;
 }
 
@@ -109,7 +120,6 @@ RTC::ReturnCode_t Translate::onShutdown(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t Translate::onActivated(RTC::UniqueId ec_id)
 {
-
   /* 行列を生成する */
   m_transformMatrix.create( 2, 3, CV_32FC1);
 
@@ -119,12 +129,6 @@ RTC::ReturnCode_t Translate::onActivated(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t Translate::onDeactivated(RTC::UniqueId ec_id)
 {
-  
-
-
-
-  
-
   return RTC::RTC_OK;
 }
 
@@ -138,44 +142,40 @@ RTC::ReturnCode_t Translate::onExecute(RTC::UniqueId ec_id)
     /* InPortデータの読み込み */
     m_image_origIn.read();
 
-
-
     /* InPortの画像データをIplImageのimageDataにコピー */
-	cv::Mat m_image_buff(cv::Size(m_image_orig.width, m_image_orig.height), CV_8UC3, (void *)&(m_image_orig.pixels[0]));
-    //memcpy(m_image_buff.data,(void *)&(m_image_orig.pixels[0]),m_image_orig.pixels.length());
-	cv::Mat m_image_dest;   /* 結果出力用IplImage */
-
-	cv::Mat m_transformMatrix;
-
+    cv::Mat m_image_buff(cv::Size(m_image_orig.width, m_image_orig.height), CV_8UC3, (void *)&(m_image_orig.pixels[0]));
+    
+    cv::Mat m_image_dest;   /* 結果出力用 */
+    cv::Mat m_transformMatrix;
 
     // Anternative process
     cv::Point2f original[3];   /* 変換前座標 */
     cv::Point2f Translate[3];  /* 変換後座標 */
 
     /* 変換前の座標を設定する */
-	original[0] = cv::Point2f(0, 0);
-	original[1] = cv::Point2f(m_image_buff.size().width, 0);
-	original[2] = cv::Point2f(0, m_image_buff.size().height);
+    original[0] = cv::Point2f(0, 0);
+    original[1] = cv::Point2f(m_image_buff.size().width, 0);
+    original[2] = cv::Point2f(0, m_image_buff.size().height);
 
     /* 変換後の座標を設定する */
-	Translate[0] = cv::Point2f(m_nTransX, m_nTransY);
-	Translate[1] = cv::Point2f(m_nTransX + m_image_buff.size().width, m_nTransY);
-	Translate[2] = cv::Point2f(m_nTransX, m_nTransY + m_image_buff.size().height);
+    Translate[0] = cv::Point2f(m_nTransX, m_nTransY);
+    Translate[1] = cv::Point2f(m_nTransX + m_image_buff.size().width, m_nTransY);
+    Translate[2] = cv::Point2f(m_nTransX, m_nTransY + m_image_buff.size().height);
 
     /* 変換行列を求める */
-	m_transformMatrix = cv::getAffineTransform(original, Translate);
+    m_transformMatrix = cv::getAffineTransform(original, Translate);
 
     /* 変換行列を反映させる */
-	warpAffine(m_image_buff, m_image_dest, m_transformMatrix, m_image_dest.size(), cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+    warpAffine(m_image_buff, m_image_dest, m_transformMatrix, m_image_dest.size(), cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
     // Common process
     /* 画像データのサイズ取得 */
-	int len = m_image_dest.channels() * m_image_dest.size().width * m_image_dest.size().height;
+    int len = m_image_dest.channels() * m_image_dest.size().width * m_image_dest.size().height;
 
     /* 画面のサイズ情報を入れる */
     m_image_output.pixels.length(len);        
-	m_image_output.width = m_image_dest.size().width;
-	m_image_output.height = m_image_dest.size().height;
+    m_image_output.width = m_image_dest.size().width;
+    m_image_output.height = m_image_dest.size().height;
 
     /* 反転した画像データをOutPortにコピー */
     memcpy((void *)&(m_image_output.pixels[0]), m_image_dest.data,len);
@@ -226,7 +226,7 @@ RTC::ReturnCode_t Translate::onRateChanged(RTC::UniqueId ec_id)
 
 extern "C"
 {
- 
+
   void TranslateInit(RTC::Manager* manager)
   {
     coil::Properties profile(translate_spec);
@@ -234,7 +234,7 @@ extern "C"
                              RTC::Create<Translate>,
                              RTC::Delete<Translate>);
   }
-  
+
 };
 
 
--- opencv/components/Translate/test/src/TranslateTestComp.cpp
+++ opencv/components/Translate/test/src/TranslateTestComp.cpp
@@ -1,6 +1,6 @@
 ﻿// -*- C++ -*-
 /*!
- * @file TranslateComp.cpp
+ * @file TranslateTestComp.cpp
  * @brief Standalone component
  * @date $Date$
  *
@@ -13,16 +13,16 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-#include "Translate.h"
+#include "TranslateTest.h"
 
 
 void MyModuleInit(RTC::Manager* manager)
 {
-  TranslateInit(manager);
+  TranslateTestInit(manager);
   RTC::RtcBase* comp;
 
   // Create a component
-  comp = manager->createComponent("Translate");
+  comp = manager->createComponent("TranslateTest");
 
   if (comp==NULL)
   {
@@ -77,6 +77,9 @@ int main (int argc, char** argv)
 {
   RTC::Manager* manager;
   manager = RTC::Manager::init(argc, argv);
+
+  // Initialize manager
+  manager->init(argc, argv);
 
   // Set module initialization proceduer
   // This procedure will be invoked in activateManager() function.
